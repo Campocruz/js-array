17acdf4c1c2a5d427b6e7cd07a41d169e70d506f
Campocruz <cosmacampo93@yahoo.it>
2026-03-05 18:57:21 +0100

    live correction

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,12 +10,22 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.toReversed();
+// const reversedTeachers = teachers.toReversed();
+const reversedTeachers = [];
+for (let i = teachers.length - 1; i >= 0; i--) {
+  reversedTeachers.push(teachers[i]);
+}
 console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter(teachers => teachers.length >= 5);
+// const longNames = teachers.filter(teachers => teachers.length >= 5);
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i])
+  }
+}
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
@@ -38,10 +48,10 @@ console.log(isFabioPresent);
 let teachersString;
 for (let i = 0; i < teachers.length; i++) {
   const subString = teachers[i];
-  if(i == 0){
+  if (i == 0) {
     teachersString = subString + ', ';
   }
-  else if(i < teachers.length - 1){
+  else if (i < teachers.length - 1) {
     teachersString = teachersString + subString + ', ';
   }
   else
